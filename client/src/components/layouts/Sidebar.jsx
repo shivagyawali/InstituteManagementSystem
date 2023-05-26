dffd459ca480318f6fbd6ca8e6960bdf8467a3a4
bipin321 <beepinpathak0721@gmail.com>
2023-05-26 15:23:09 +0545

    working on dashboard

--- client/src/components/layouts/Sidebar.jsx
+++ client/src/components/layouts/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
                 </ul>
                 <div>
                     <div onClick={() => (setMenuPop(true))}
-                        className={` w-10 h-10 text-2xl font-bold flex items-center justify-center  bg-dark rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11 hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${menuPop ? 'hidden' : 'block'}`}>
+                        className={`w-10 h-10 text-2xl font-bold flex items-center justify-center  bg-dark rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11  hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${menuPop ? 'hidden' : 'block'}`}>
                         <BiDotsHorizontalRounded />
                     </div>
                     <div onClick={() => (setMenuPop(false))}
@@ -30,7 +30,7 @@ const Sidebar = () => {
                         <GrFormClose />
                     </div> 
 
-                    <div className={` fixed bottom-14 left-10 bg-dark py-6 rounded-md text-lg transition-all ease-in-out duration-100 shadow-md shadow-gray-500  ${menuPop ? 'opacity-1 ' : 'opacity-0'}`}>
+                    <div className={`fixed bottom-14 left-10 bg-dark py-6 rounded-md text-lg transition-all ease-in-out duration-100 shadow-md shadow-gray-500  ${menuPop ? 'opacity-1 ' : 'opacity-0'}`}>
                         <ul>
                             <li className='hover:bg-yellow-500 w-full px-8 cursor-pointer py-1 flex items-center gap-2'> <BiUserCircle />Profile</li>
                             <li className='hover:bg-yellow-500 w-full px-8 cursor-pointer py-1 flex items-center gap-2'> <FiSettings />Settings</li>
