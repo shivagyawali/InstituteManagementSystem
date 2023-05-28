5047915f7f4997b18e55c79b8b0845ea570eca3b
shivagyawali <shiv.alfa8@gmail.com>
2023-05-28 11:34:09 +0545

    push main

--- client/src/components/layouts/Sidebar.jsx
+++ client/src/components/layouts/Sidebar.jsx
@@ -12,49 +12,46 @@ const Sidebar = () => {
       <div className="bg-primary h-screen w-52 flex flex-col justify-between p-4">
         <p className="text-3xl font-bold px-4 ">IMS</p>
         <ul className="flex flex-col gap-2 w-full  ">
-          <li className="sidebar-link">Institute</li>
-          <li className="sidebar-link">Course</li>
-          <li className="sidebar-link">Teachers</li>
-          <li className="sidebar-link">Students</li>
-          <li className="sidebar-link z">Staff</li>
+          <li className="sidebar-link hover:text-gray-200">Institute</li>
+          <li className="sidebar-link hover:text-gray-200">Course</li>
+          <li className="sidebar-link hover:text-gray-200">Teachers</li>
+          <li className="sidebar-link hover:text-gray-200">Students</li>
+          <li className="sidebar-link hover:text-gray-200 z">Staff</li>
         </ul>
         <div>
           <div
             onClick={() => setMenuPop(true)}
-            className={` w-10 h-10 text-2xl font-bold flex items-center justify-center  bg-dark rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11 hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${
+            className={` w-10 h-10 text-2xl font-bold flex items-center justify-center  bg-gray-200 rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11 hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${
               menuPop ? "hidden" : "block"
             }`}
           >
-            <BiDotsHorizontalRounded />
+            <BiDotsHorizontalRounded onClick={() => setMenuPop(true)} />
           </div>
           <div
             onClick={() => setMenuPop(false)}
-            className={`z-10 w-10 h-10 text-2xl font-bold flex items-center justify-center  bg-dark rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11 hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${
+            className={`z-10 w-10 h-10 text-2xl font-bold flex items-center justify-center bg-gray-200 rounded-full shadow-md shadow-gray-600 cursor-pointer hover:w-11 hover:h-11 hover:text-3xl fixed bottom-10 transition-all ease-in duration-200 ${
               menuPop ? "block" : "hidden"
             }`}
           >
-            <GrFormClose />
+            <GrFormClose onClick={() => setMenuPop(false)} />
           </div>
 
           <div
-            className={` fixed bottom-14 left-10 bg-dark py-6 rounded-md text-lg transition-all ease-in-out duration-100 shadow-md shadow-gray-500  ${
+            className={` fixed bottom-14 left-10 bg-gray-200 py-3 rounded-md text-lg transition-all ease-in-out duration-100 shadow-md shadow-gray-500  ${
               menuPop ? "opacity-1 " : "opacity-0"
             }`}
           >
             <ul>
               <li className="hover:bg-yellow-500 w-full px-8 cursor-pointer py-1 flex items-center gap-2">
-                {" "}
-                <BiUserCircle />
+                <BiUserCircle className="w-4 h-4 " />
                 Profile
               </li>
               <li className="hover:bg-yellow-500 w-full px-8 cursor-pointer py-1 flex items-center gap-2">
-                {" "}
-                <FiSettings />
+                <FiSettings className="w-4 h-4 " />
                 Settings
               </li>
               <li className="hover:bg-yellow-500 w-full px-8 cursor-pointer py-1 flex items-center gap-2">
-                {" "}
-                <AiOutlineLogout />
+                <AiOutlineLogout className="w-4 h-4 " />
                 Logout
               </li>
             </ul>
