3062165dfa1b188f20e4d0757f2df44e38cfda81
bipin321 <beepinpathak0721@gmail.com>
2023-05-31 12:16:48 +0545

    pull

--- client/src/components/layouts/Header.jsx
+++ client/src/components/layouts/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = ({headerText}) => {
     return (
-        <div>
-            <p className='lg:text-3xl md:text-2xl sm:text-xl text-lg mb-5 sticky lg:relative top-0  lg:top-3  bg-primary p-3 lg:rounded-md text-center lg:inline-block'
+        <div className='sticky lg:relative top-0  lg:top-3 '>
+            <p className='lg:text-3xl md:text-2xl sm:text-xl text-lg mb-5 bg-primary p-3 lg:rounded-md text-center lg:inline-block'
             >
                {headerText}
             </p >
